0a9cc994a7f745ef36577bcfef4a4fe66b40db00
Daehwan Lee <eoghks1703@gmail.com>
2023-07-05 08:09:20 +0900

    Navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,49 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Col,
+  Nav,
+  Navbar,
+  Container,
+  Row,
+  Form,
+  Button,
+} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header>
+        <Navbar
+          id="search-nav"
+          bg="white"
+          variant="white"
+          className="flex-column flex-md-row  p-2"
         >
-          Learn React
-        </a>
+          <Container fluid id="search-nav-container">
+            <Navbar.Brand href="/">
+              <div className="link-logo">
+                <span className="logo-line-1">News</span>
+                <span className="logo-line-2">Reader</span>
+              </div>
+            </Navbar.Brand>
+            <Form id="home-search-form" className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                id="home-search-control"
+              />
+            </Form>
+            <Nav>
+              <Nav.Link href="/signin" className="nav-right-item">
+                <Button variant="primary" className="home-sign-in">
+                  Sign in
+                </Button>
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
       </header>
     </div>
   );
